b808b35d6339e5c9b09d8681ea65b1a885571cad
Peter Williams <peterkennethwilliams@gmail.com>
2024-08-05 16:03:06 +0100

    Program entry point
    Program now looks for and requires an url passed in as a starting
    condition.

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+import { argv } from "node:process";
+import { crawlPage } from "./src/crawl.js";
+import { printReport } from "./src/report.js";
+async function main() {
+  try {
+    if (argv.length < 3) {
+      throw new Error("Missing required argument: <url>");
+    }
+    if (argv.length > 3) {
+      throw new Error("Too many arguments command requests one: <url>");
+    }
+
+    const url = argv[2];
+    try {
+      new URL(url);
+    } catch {
+      throw new Error("Invalid URL");
+    }
+    const html = await crawlPage(url);
+    printReport(html);
+  } catch (error) {
+    console.log(`error: ${error.message}`);
+    process.exit(1);
+  }
+}
+main();
